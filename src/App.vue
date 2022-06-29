f06d8e5981506eaa8b5f73bd1dc984560c4dcb4d
glebmatveev <glebmatveev@gmail.com>
2022-06-29 11:05:44 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import MainHeader from "./components/MainHeader.vue";
+import MainFooter from "./components/MainFooter.vue";
+
+export default {
+  components: {
+    MainHeader,
+    MainFooter,
+  },
+};
+</script>
+
+<template>
+  <MainHeader></MainHeader>
+  <div class="div_background">
+    <p class="p__text">Hello!</p>
+  </div>
+  <h1>world</h1>
+
+  <h2>champion</h2>
+  <MainFooter></MainFooter>
+</template>
+
+<style lang="scss">
+@import "@/assets/styles/fonts.scss";
+@import "@/assets/styles/variables.scss";
+
+h2 {
+  color: $bg-color1;
+}
+</style>
---
+++ src/components/MainFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="div_background">
+    <p class="p__text">Hello!</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/Home/main-footer.scss";
+</style>
